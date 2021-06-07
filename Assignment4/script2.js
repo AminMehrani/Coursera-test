db5c8e12f50b76f8b3eed175154d9f3bf76cc85b
Tbag29203 <Tbag29203@yahoo.com>
2021-06-07 17:42:58 +0430

    fine

---
+++ Assignment4/script2.js
@@ -0,0 +1,6 @@
+var x = new Object();
+x.name= "boluke 7"
+x.name = new Object ();
+x.name.firstname= "Amin"
+x.name.lastname= "Mehrani"
+console.log("Hello There: " + x.name.firstname + " " + x.name.lastname);
